425d420a8e60818b2ea70a63e4797e13550f6c06
Leonardo Sousa <leosousa84@gmail.com>
2021-08-29 12:02:09 -0300

    Add files via upload
    Script para remover acentos e novo wordlist

---
+++ replace-char.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import unicodedata
+import sys
+
+def strip_accents(text):
+    try:
+        text = unicode(text, 'utf-8')
+    except NameError: # unicode is a default on python 3 
+        pass
+
+    text = unicodedata.normalize('NFD', text)\
+           .encode('ascii', 'ignore')\
+           .decode("utf-8")
+
+    return str(text)
+
+arquivo = sys.argv[1]
+
+palavra = open(arquivo, 'r')
+with open(arquivo, mode='r', encoding='ascii') as f:
+    for i in f:
+        s = strip_accents(i)
+        linew = open('wl-novo', mode='a+', encoding='utf-8')
+        linew.writelines(s)
+        print(s)
